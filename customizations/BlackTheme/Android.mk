7554a5e78d2cb11526b5cf0a554e87dc0f58d3eb
Simao Gomes Viana <simao@halogenos.org>
2022-06-30 16:02:35 +0200

    Delineagify

--- customizations/BlackTheme/Android.mk
+++ customizations/BlackTheme/Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := LineageBlackTheme
+LOCAL_RRO_THEME := CustomBlackTheme
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := LineageBlackTheme
+LOCAL_PACKAGE_NAME := CustomBlackTheme
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
--- customizations/NavigationBarNoHint/Android.mk
+++ customizations/NavigationBarNoHint/Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := LineageNavigationBarNoHint
+LOCAL_RRO_THEME := CustomNavigationBarNoHint
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := LineageNavigationBarNoHint
+LOCAL_PACKAGE_NAME := CustomNavigationBarNoHint
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
